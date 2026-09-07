b3a73220db91ece3b64e158f272e508a99934705
carloangkisann <19623005@std.stei.itb.ac.id>
2026-09-07 07:03:45 +0700

    chore: add build_push_image_karsajobs_ui.sh

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build the Docker image from the Dockerfile
+docker build -t macchiato21/karsajobs-ui:latest .
+
+# Log in to Docker Hub using the password from an environment variable
+echo $PASSWORD_DOCKER_HUB | docker login -u macchiato21 --password-stdin
+
+# Push the image to Docker Hub
+docker push macchiato21/karsajobs-ui:latest
